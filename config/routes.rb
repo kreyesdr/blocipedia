c7baeb26c46b965c1d2d4250ba46ff2131e99874
kreyesdr <kreyes616@gmail.com>
2016-08-13 21:46:43 -0400

    Fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -54,7 +57,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  root 'welcome#index'
-
 end
